8b75257d2fa25d9cea17644f15a08b8c2ae79a57
San <sanowl98@gmail.com>
2025-06-25 14:03:26 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/SourceMindLabs.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/SourceMindLabs.github.io' : '',
 };
 
 export default nextConfig;
